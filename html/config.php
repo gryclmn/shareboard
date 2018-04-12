8d7e06ab11ecb606490673ee23e10aeaa4f5e42e
Gary Coleman <gryclmn@hotmail.com>
2018-04-12 10:29:16 -0700

    Finished Udemy course "Learn Object Oriented PHP by Building a Complete Website"

---
+++ html/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Define DB Params
+		define("DB_HOST", "localhost");
+		define("DB_USER", "");
+		define("DB_PASS", "");
+		define("DB_NAME", "shareboard");
+		
+		// Define URL
+		define("ROOT_PATH", "/");
+//		define("ROOT_URL", "http:localhost/10.223.5.8/");
+		define("ROOT_URL", "/");
+
+?>
